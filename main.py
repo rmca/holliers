eb62f099f5be7e9ef953ce9c033c05d7832a565c
Rob <rob.mcadoo@gmail.com>
2018-01-31 21:32:42 +0000

    First working version + show command skeleton

--- main.py
+++ main.py
@@ -1,26 +1,85 @@
 from pyPdf import PdfFileWriter, PdfFileReader
-from StringIO import StringIO
+import StringIO
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
 
-def overlay_text_at_loc_on_pdf(text, loc, existing_pdf, output_pdf):
+def overlay_text_at_loc_on_pdf(texts, original_pdf, output_pdf):
+    packet = StringIO.StringIO()
     # create a new PDF with Reportlab
-    packet = StringIO()
     can = canvas.Canvas(packet, pagesize=letter)
-    can.drawString(loc[0],loc[1], text)
+    for text, loc in texts:
+    	can.drawString(loc[0], loc[1], text)
     can.save()
 
     #move to the beginning of the StringIO buffer
     packet.seek(0)
-    new_pdf = PdfFileReader(existing_pdf)
+    new_pdf = PdfFileReader(packet)
     # read your existing PDF
+    existing_pdf = PdfFileReader(file(original_pdf, "rb"))
     output = PdfFileWriter()
-    page = new_pdf.getPage(0)
+    # add the "watermark" (which is the new pdf) on the existing page
+    page = existing_pdf.getPage(0)
     page.mergePage(new_pdf.getPage(0))
     output.addPage(page)
     # finally, write "output" to a real file
-    output.write(output_pdf)
+    outputStream = file(output_pdf, "wb")
+    output.write(outputStream)
+    outputStream.close()
+   
 
-with open('newpdf2.pdf', 'wb') as fp:
-    overlay_text_at_loc_on_pdf("Robert McAdoo", (160, 650,), file('holidays.pdf', 'rb'), fp)
+texts = [
+	("Robert McAdoo", (130, 640,),),
+	("Nicola Asuni", (130, 620,),),
+	("x", (45, 545,),),
+	("20 days", (295, 475,),),
+	("From date", (205, 425,),),
+	("To date", (405, 425,),),
+]
+
+
+import click
+from click_datetime import Datetime
+
+
+@click.group()
+def cli():
+    pass
+
+def make_form_content(name, manager, num_days, from_date, to_date):
+    return [
+        (name, (130, 640,),),
+        (manager, (130, 620,),),
+        ("x", (45, 545,),),
+        ("{} days".format(num_days), (295, 475,),),
+        (from_date, (205, 425,),),
+        (to_date, (405, 425,),),
+    ] 
+
+@click.command()
+@click.argument('name', type=str, required=True)
+@click.argument('manager', type=str, required=True)
+@click.argument('num_days', type=int, required=True)
+@click.argument('from_date', type=Datetime(format='%d/%m/%y'), required=True)
+@click.argument('to_date', type=Datetime(format='%d/%m/%y'), required=True)
+@click.option('--output-file', type=click.Path(exists=False), default='holiday_request.pdf')
+@click.option('--template-file', type=click.Path(exists=True), default='holidays.pdf')
+def holiday(name, manager, num_days, from_date, to_date, output_file, template_file):
+    """
+    Holidays for the lazy.
+
+    Fills out a holiday request form on your behalf so you don't have to.
+    """
+    texts = make_form_content(name, manager, num_days, from_date.strftime('%d/%m/%y'), to_date.strftime('%d/%m/%y'))
+    overlay_text_at_loc_on_pdf(texts, template_file, output_file)
+
+
+@click.command()
+@click.option('--form-file', default='holida_request.pdf')
+def show(ctx, form_file):
+    pass 
+
+cli.add_command(holiday)
+
+if __name__ == '__main__':
+    cli({})
